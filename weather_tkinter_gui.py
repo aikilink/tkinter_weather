f3ae8bf947d822a855c1516a533ebba7a992cf98
aikilink <quintinrg@gmail.com>
2019-05-28 10:36:36 -0500

    Fixed formatting and imported fonts to improve UI. Also added exception handling

--- weather_tkinter_gui.py
+++ weather_tkinter_gui.py
@@ -1,6 +1,7 @@
 # On Github/aikilink
 
 import tkinter as tk
+from tkinter import font
 import requests
 
 HEIGHT = 500
@@ -22,11 +23,17 @@ def get_weather(city):
     label['text'] = format_response(weather)
 
 def format_response(weather):
-    name = weather['name']
-    desc = weather['weather'][0]['description']
-    temp = weather['main']['temp']
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
 
-    label['text'] = str(name) + ', ' + str(desc) + ': ' + str(temp)
+        final_str = 'City: %s \n Conditions: %s \n Temperature (°F): %s' %(name, desc, temp)
+    
+    except:
+        final_str = 'There was a problem retrieving that information'
+    
+    return final_str
 
 root = tk.Tk()
 
@@ -40,16 +47,16 @@ background_label.place(relwidth = 1, relheight = 1)
 frame = tk.Frame(root, bg = '#60c1aa', bd = 5)
 frame.place(relx = 0.5, rely = 0.1, relwidth = 0.75, relheight = 0.1, anchor = 'n')
 
-entry = tk.Entry(frame, font = '40')
+entry = tk.Entry(frame, font = ('Courier', 16))
 entry.place(relwidth = 0.65, relheight = 1)
 
-button = tk.Button(frame, text = "Get Weather", bg = '#80c1df', font = 40, command = lambda: get_weather(entry.get()))
+button = tk.Button(frame, text = "Get Weather", bg = '#80c1df', font = ('Courier', 12), command = lambda: get_weather(entry.get()))
 button.place(relx = 0.7, relheight = 1, relwidth = 0.3)
 
 lower_frame = tk.Frame(root, bg = '#60c1aa', bd = 10)
 lower_frame.place(relx = 0.5, rely = 0.25, relwidth = 0.75, relheight = 0.6, anchor = 'n')
 
-label = tk.Label(lower_frame)
+label = tk.Label(lower_frame, font = ('Courier', 18))
 label.place(relwidth = 1, relheight = 1)
 
 root.mainloop()
